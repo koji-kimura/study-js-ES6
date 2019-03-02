019cf8ec58b898fa2efb0735324a6e1064c63782
kimura <37994567+koji-kimura@users.noreply.github.com>
2019-03-02 09:07:09 +0900

    本日勉強分
    forEachについて。Eは大文字

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-// 配列の便利メソッド
+// fpreach実践編
+var numbers = [1, 2, 3, 4, 5];
 
-// forで記載しているもの、でも今後は記載しないようにしていく
-// 冗長だから
-var colors = ["red", "blue", "green"];
-// for (var i = 0; i < colors.length; i++) {
-//   console.log("for:" + colors[i]);
-// }
+// 合計を保持する変数を用意する
+var sum = 0;
+var add = number => {
+  sum += number;
+};
 
-colors.forEach(function(color) {
-  console.log(color);
-});
+// 配列を１つ１つ足す
+// 名前付きの関数を入れたら()はつけない
+numbers.forEach(add);
+
+// 合計を表示する
+console.log(sum);
