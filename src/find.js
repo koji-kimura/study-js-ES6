96b9de132fef74d98b6b05c1b3d49e06fc184332
kimura <37994567+koji-kimura@users.noreply.github.com>
2019-03-06 07:11:50 +0900

    本日勉強分
    findについて

--- src/find.js
+++ src/find.js
@@ -1,16 +1,27 @@
-var users = [{ name: "太郎" }, { name: "二郎" }, { name: "三郎" }];
+function Car(model) {
+  this.model = model;
+}
 
-var user;
+var cars = [new Car("プリウス"), new Car("ノート"), new Car("アクア")];
 
-// for (var i = 0; i < users.length; i++) {
-//   if (users[i].name === "二郎") {
-//     user = users[i];
-//     break;
-//   }
-// }
-
-user = users.find(function(user) {
-  return user.name === "二郎";
+var car_search = cars.find(car => {
+  return car.model === "アクア";
 });
 
-console.log(user);
+console.log(car_search);
+// var users = [{ name: "太郎" }, { name: "二郎" }, { name: "三郎" }];
+
+// var user;
+
+// // for (var i = 0; i < users.length; i++) {
+// //   if (users[i].name === "二郎") {
+// //     user = users[i];
+// //     break;
+// //   }
+// // }
+
+// user = users.find(function(user) {
+//   return user.name === "二郎";
+// });
+
+// console.log(user);
