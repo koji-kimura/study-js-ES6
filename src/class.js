dc625151f607b71be395edb14472f013231cd30f
kimura <37994567+koji-kimura@users.noreply.github.com>
2019-03-19 08:07:23 +0900

    本日勉強分

--- src/class.js
+++ src/class.js
@@ -1,3 +1,35 @@
+// これでクラスを作る
+class Car {
+  // 分割代入
+  constructor({ title }) {
+    this.title = title;
+  }
+  // カンマは入れない
+  drive() {
+    return "ウィーン";
+  }
+}
+
+class Toyota extends Car {
+  constructor(options) {
+    super(options); // carコンストラクタを呼んでいる
+    this.color = options.color;
+  }
+  honk() {
+    return "ぶぶー";
+  }
+}
+
+const toyota = new Toyota({ color: "red", title: "アクア" });
+console.log(toyota.honk());
+console.log(toyota);
+console.log(toyota.drive());
+
+// const car = new Car({ title: "アクア" });
+
+// console.log(car);
+// console.log(car.drive());
+
 // 以下はES5の書き方
 // function Car(options) {
 //   this.title = options.title;
