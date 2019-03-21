2af4c47e2ecf610cd5676bed4df9e597740a4d39
kimura <37994567+koji-kimura@users.noreply.github.com>
2019-03-21 21:27:41 +0900

    本日勉強分
    promise理解でき始めてる

--- src/promise.js
+++ src/promise.js
@@ -1,2 +1,24 @@
+// 何もインポートしないでも動く
+// 中になんか書いておかないと
+const promise = new Promise((resolve, reject) => {
+  // resolve(alert(this));
+  reject();
+});
+
+// 成功の時の処理
+promise.then(() => {
+  console.log("実行しました");
+});
+
+// 失敗の時の処理
+promise.catch(() => {
+  console.log("失敗しました");
+});
+
+// console.log(promise);
+// promiseとajaxは一緒にいるわけではない
 // jsにはコードを眠らせておく機能はない
 // ノンブロッキング
+// コードを止めておく動きがjsにはない
+// これをいい感じにしてくれるのがpromise
+// 「--きてから」は（then）
