1f9f8ab8d5576eb432e7f85538fd96f0bafa0113
kimura <37994567+koji-kimura@users.noreply.github.com>
2019-04-12 07:46:44 +0900

    本日勉強分
    findとfitlerについて。

--- src-3/filter.js
+++ src-3/filter.js
@@ -1,21 +1,35 @@
-var products = [
-  { name: "きゅうり", type: "野菜", quantity: 0, price: 1 },
-  { name: "バナナ", type: "フルーツ", quantity: 10, price: 15 },
-  { name: "セロリ", type: "野菜", quantity: 30, price: 9 },
-  { name: "オレンジ", type: "フルーツ", quantity: 3, price: 5 }
+var post = { id: 4, title: "初めての投稿" };
+var comments = [
+  { postId: 4, content: "いい記事ですね" },
+  { postId: 3, content: "勉強になりました" },
+  { postId: 4, content: "なるほど" }
 ];
 
-let filteredProducts = [];
+function commentsForPost(post, comments) {
+  return comments.filter(comment => {
+    return comment.postId === post.id;
+  });
+}
 
-// 種類が野菜　量が0より多い、値段が10より小さい
-// 分割代入もキメてみた
-filteredProducts = products.filter(({ name, type, quantity, price }) => {
-  return type === "野菜" && quantity > 0 && price > 0;
-});
-// for (let i = 0; i < products.length; i++) {
-//   if (products[i].type === "フルーツ") {
-//     filteredProducts.push(products[i]);
-//   }
-// }
+console.log(commentsForPost(post, comments));
+// var products = [
+//   { name: "きゅうり", type: "野菜", quantity: 0, price: 1 },
+//   { name: "バナナ", type: "フルーツ", quantity: 10, price: 15 },
+//   { name: "セロリ", type: "野菜", quantity: 30, price: 9 },
+//   { name: "オレンジ", type: "フルーツ", quantity: 3, price: 5 }
+// ];
 
-console.log(filteredProducts);
+// let filteredProducts = [];
+
+// // 種類が野菜　量が0より多い、値段が10より小さい
+// // 分割代入もキメてみた
+// filteredProducts = products.filter(({ name, type, quantity, price }) => {
+//   return type === "野菜" && quantity > 0 && price > 0;
+// });
+// // for (let i = 0; i < products.length; i++) {
+// //   if (products[i].type === "フルーツ") {
+// //     filteredProducts.push(products[i]);
+// //   }
+// // }
+
+// console.log(filteredProducts);
---
+++ src-3/find.js
@@ -0,0 +1,22 @@
+function Car(model) {
+  this.model = model;
+}
+
+var cars = [new Car("プリウス"), new Car("ノート"), new Car("アクア")];
+
+// var users = [{ name: "太郎" }, { name: "二郎" }, { name: "三郎" }];
+
+// var user;
+
+// // 最初に見つかった時点で終了する
+// user = users.find(user => {
+//   return user.name === "二郎";
+// });
+// // for (var i = 0; i < users.length; i++) {
+// //   if (users[i].name === "二郎") {
+// //     user = users[i];
+// //     break;
+// //   }
+// // }
+
+// console.log(user);
