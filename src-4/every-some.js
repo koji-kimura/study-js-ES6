b4cccf8ebca9cc3375a04329fa93b47f5e2cd05f
kimura <37994567+koji-kimura@users.noreply.github.com>
2019-05-07 08:13:19 +0900

    本日勉強分
    reduceとかevery・someについて

--- src-4/every-some.js
+++ src-4/every-some.js
@@ -1,34 +1,47 @@
-var computers = [
-  { name: "Apple", ram: 24 },
-  { name: "Compaq", ram: 4 },
-  { name: "Acer", ram: 32 }
-];
+var names = ["けん", "はなこ", "そういちろう"];
 
-//16GB rqamが必要なソフト
-
-// すべてで動かせるフラグ
-var allComputersCanRun = true;
-
-// どれかでは動かせるフラグ
-var someComputersCanRun = false;
-
-// for (var i = 0; i < computers.length; i++) {
-//   var computer = computers[i];
-
-//   if (computer.ram < 16) {
-//     allComputersCanRun = false;
-//   } else {
-//     someComputersCanRun = true;
-//   }
-// }
-
-allComputersCanRun = computers.every(computer => {
-  return computer.ram >= 16;
+let x = names.every(function(name) {
+  return name.length >= 3;
 });
 
-someComputersCanRun = computers.some(computer => {
-  return computer.ram >= 16;
+let y = names.some(function(name) {
+  return name.length >= 3;
 });
 
-console.log(allComputersCanRun);
-console.log(someComputersCanRun);
+console.log(x);
+console.log(y);
+
+// var computers = [
+//   { name: "Apple", ram: 24 },
+//   { name: "Compaq", ram: 4 },
+//   { name: "Acer", ram: 32 }
+// ];
+
+// //16GB rqamが必要なソフト
+
+// // すべてで動かせるフラグ
+// var allComputersCanRun = true;
+
+// // どれかでは動かせるフラグ
+// var someComputersCanRun = false;
+
+// // for (var i = 0; i < computers.length; i++) {
+// //   var computer = computers[i];
+
+// //   if (computer.ram < 16) {
+// //     allComputersCanRun = false;
+// //   } else {
+// //     someComputersCanRun = true;
+// //   }
+// // }
+
+// allComputersCanRun = computers.every(computer => {
+//   return computer.ram >= 16;
+// });
+
+// someComputersCanRun = computers.some(computer => {
+//   return computer.ram >= 16;
+// });
+
+// console.log(allComputersCanRun);
+// console.log(someComputersCanRun);
